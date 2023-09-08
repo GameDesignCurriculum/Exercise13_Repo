24cd71a49e033490eb1712294be4b6eb40e900ca
Patricia Gallant <Patricia.Gallant@neric.org>
2023-09-07 20:07:31 -0400

    Added starter code materials

---
+++ Exercise-13-Materials/TeddyBear.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A teddy bear
+/// </summary>
+public class TeddyBear : MonoBehaviour
+{
+	/// <summary>
+    /// Start is called before the first frame update
+    /// </summary>
+	void Start()
+    {
+        // get the teddy bear moving
+
+    }
+	
+}
